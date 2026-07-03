bf0137b0e0991d24826ce5628b0e98c950694dd2
freeideas <freeideas@users.noreply.github.com>
2026-07-03 15:19:52 +0200

    WriteCode proj/subpjx/DryRunMode: done

--- proj/subpjx/DryRunMode/src/imp.rs
+++ proj/subpjx/DryRunMode/src/imp.rs
@@ -1,23 +1,78 @@
 use std::sync::Arc;
+
 use crate::api::*;
 
 struct DryRunModeImpl;
 
 impl DryRunMode for DryRunModeImpl {
     fn dry_run_output_line(&self) -> String {
-        unimplemented!()
+        "dry run".to_string()
     }
-    fn startup_root_decision( &self, scheme: DryRunModePeerScheme, root_state: DryRunModeRootState, ) -> DryRunModeStartupRootDecision {
-        unimplemented!()
+
+    fn startup_root_decision(
+        &self,
+        _scheme: DryRunModePeerScheme,
+        root_state: DryRunModeRootState,
+    ) -> DryRunModeStartupRootDecision {
+        match root_state {
+            DryRunModeRootState::Exists => DryRunModeStartupRootDecision::UseExistingRoot,
+            DryRunModeRootState::Missing => {
+                DryRunModeStartupRootDecision::FailCandidateWithoutCreatingRoot
+            }
+        }
     }
-    fn snapshot_startup_decision( &self, outcome: DryRunModeSnapshotDownloadOutcome, ) -> DryRunModeSnapshotStartupDecision {
-        unimplemented!()
+
+    fn snapshot_startup_decision(
+        &self,
+        outcome: DryRunModeSnapshotDownloadOutcome,
+    ) -> DryRunModeSnapshotStartupDecision {
+        match outcome {
+            DryRunModeSnapshotDownloadOutcome::Found => {
+                DryRunModeSnapshotStartupDecision::UseLiveSnapshotAsLocalTemporary
+            }
+            DryRunModeSnapshotDownloadOutcome::NotFound => {
+                DryRunModeSnapshotStartupDecision::CreateEmptyLocalTemporarySnapshot
+            }
+            DryRunModeSnapshotDownloadOutcome::Failed => {
+                DryRunModeSnapshotStartupDecision::ExcludePeerWithErrorDiagnostic
+            }
+        }
     }
+
     fn classify_work(&self, work: DryRunModeWorkKind) -> DryRunModeWorkDecision {
-        unimplemented!()
+        match work {
+            DryRunModeWorkKind::ConnectToExistingRoot
+            | DryRunModeWorkKind::ListDirectory
+            | DryRunModeWorkKind::StatPath
+            | DryRunModeWorkKind::DownloadSnapshot
+            | DryRunModeWorkKind::ReadSourceFile => DryRunModeWorkDecision::AllowPeerRead,
+            DryRunModeWorkKind::CreateOrUpdateLocalTemporarySnapshot => {
+                DryRunModeWorkDecision::AllowLocalWorkingWrite
+            }
+            DryRunModeWorkKind::CreatePeerDirectory
+            | DryRunModeWorkKind::CreatePeerMetadataDirectory
+            | DryRunModeWorkKind::WritePeerFile
+            | DryRunModeWorkKind::RenamePeerEntry
+            | DryRunModeWorkKind::DeletePeerEntry
+            | DryRunModeWorkKind::DisplacePeerEntryToBak
+            | DryRunModeWorkKind::SetPeerModificationTime
+            | DryRunModeWorkKind::RecoverPeerSnapshotSwap
+            | DryRunModeWorkKind::RecoverPeerUserFileSwap
+            | DryRunModeWorkKind::CleanPeerBakTmp
+            | DryRunModeWorkKind::UploadPeerSnapshot => {
+                DryRunModeWorkDecision::SuppressPeerWritePlannedSuccess
+            }
+        }
     }
+
     fn copy_work_policy(&self) -> DryRunModeCopyWorkPolicy {
-        unimplemented!()
+        DryRunModeCopyWorkPolicy {
+            acquire_copy_slots: true,
+            read_sources: true,
+            apply_normal_retry_limit: true,
+            emit_copy_progress: true,
+            emit_delete_progress: true,
+        }
     }
 }
 
